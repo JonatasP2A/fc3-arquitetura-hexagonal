95274a744c2b087dcc35fda2dd0c81d315aa5d4d
Jonatas Alves <jonatasalves@id.uff.br>
2024-08-27 00:15:04 -0300

    feat: preparando testes de banco de dados

---
+++ adapters/db/product_test.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products (
+							"id" string,
+							"name" string,
+							"price" float,
+							"status" string
+						);`
+
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `INSERT INTO products (id, name, price, status) VALUES (?, ?, ?, ?)`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	_, err = stmt.Exec("1", "Product 1", 10.5, "enabled")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
